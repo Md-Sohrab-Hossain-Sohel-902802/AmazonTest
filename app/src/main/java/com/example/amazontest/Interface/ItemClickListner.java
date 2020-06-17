2c1d3dd9f69874d912fb03027c46a88969811428
Sohrab <mdsohrabhossainsohel902833@gmail.com>
2020-06-17 07:02:42 -0700

    Initial commit

---
+++ app/src/main/java/com/example/amazontest/Interface/ItemClickListner.java
@@ -0,0 +1,13 @@
+package com.example.amazontest.Interface;
+
+import android.view.View;
+
+public interface ItemClickListner {
+
+
+    void onClick(View view,int position,boolean  isLongClick);
+
+
+
+
+}
